a59b80f333d7d69255bbc6c4a0943930e49266fb
timweissenfels <timweissenfels7@gmail.com>
2025-10-30 01:52:48 +0100

    Added ActivationTypes Class

---
+++ NeuralNetwork/ActivationTypes.h
@@ -0,0 +1,16 @@
+//
+// Created by timwe on 10/29/2025.
+//
+
+#ifndef NEUROINFORMATICS_ACTIVATIONTYPES_H
+#define NEUROINFORMATICS_ACTIVATIONTYPES_H
+
+namespace NeuralNetwork {
+
+    enum class ActivationTypes {
+        Relu, Sigmoid, Softplus, Delu, Elu, Tanh, Selu
+    };
+
+}
+
+#endif //NEUROINFORMATICS_ACTIVATIONTYPES_H
