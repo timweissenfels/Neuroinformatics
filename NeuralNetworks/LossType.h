b8c44de534839e27dddf5450384733b0ab25b559
timweissenfels <timweissenfels7@gmail.com>
2025-10-31 01:44:46 +0100

    Added a LossType enunm class as a helper for the NeuralNetwork class

---
+++ NeuralNetworks/LossType.h
@@ -0,0 +1,14 @@
+//
+// Created by timwe on 10/31/2025.
+//
+
+#ifndef NEUROINFORMATICS_LOSSTYPE_H
+#define NEUROINFORMATICS_LOSSTYPE_H
+
+namespace NeuralNetworks {
+    enum class LossType {
+        BCE, MSE
+    };
+}
+
+#endif //NEUROINFORMATICS_LOSSTYPE_H
