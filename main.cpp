c528e49c2108d646dd50c88f15e498723c1c7374
timweissenfels <timweissenfels7@gmail.com>
2025-11-02 00:25:52 +0100

    Basic working example for sin task

--- main.cpp
+++ main.cpp
@@ -1,86 +1,35 @@
 #include <iostream>
+#include <cmath>
+#include <chrono>
+#include "NeuralNetworks/NeuralNetwork.h"
+#include "NeuralNetworks/LossType.h"
 #include "Math/Matrix.h"
-// -O3 -DNDEBUG for speed; -DML_DEBUG to enable shape asserts. -mcpu=apple-m1 -ffast-math
-
-bool test_hadamard_product() {
-    auto A = Math::Matrix<double>(3, 2);
-    auto B = Math::Matrix<double>(3, 2);
-    auto C = Math::Matrix<double>(3, 2);
-
-    A(0, 0) = 2; A(0,1) = 4;
-    A(1, 0) = 1; A(1,1) = 3;
-    A(2, 0) = 5; A(2,1) = 2;
-
-    B(0, 0) = 3; B(0,1) = 1;
-    B(1, 0) = 2; B(1,1) = 4;
-    B(2, 0) = 1; B(2,1) = 6;
-
-    C(0, 0) = 6; C(0,1) = 4;
-    C(1, 0) = 2; C(1,1) = 12;
-    C(2, 0) = 5; C(2,1) = 12;
-
-   auto C_check = A.hadamard(B);
-
-   if(std::equal(std::begin(C_check.data()), std::end(C_check.data()), std::begin(C.data()), std::end(C.data())))
-        return true;
-   else
-       return false;
-}
 
 int main() {
-    auto A = Math::Matrix<double>(3, 2);
-    auto B = Math::Matrix<double>(3, 2);
+    auto X = Math::Matrix<float>(1,71,0);
+    auto Y = Math::Matrix<float>(1,71,0);
 
-    A.fill(5);
-    B.fill(9);
+    for(int i = 0; i <= 70; i++) {
+        X(0, i) = (float)i/10;
+        Y(0,i) = sinf((float)i/10);
+    }
 
-    std::cout << A << std::endl;
-    std::cout << B << std::endl;
+    NeuralNetworks::NeuralNetwork sinNN(NeuralNetworks::LossType::MSE, (double)0.09, 1000, 32, 42, X);
+    sinNN.AddDenseLayer(1,4,NeuralNetworks::ActivationTypes::Tanh);
+    sinNN.AddDenseLayer(4,1,NeuralNetworks::ActivationTypes::Linear);
 
-    auto result = A.add(B);
+    auto start = std::chrono::high_resolution_clock::now();
 
-    std::cout << result << std::endl;
+    for (int step = 0; step < 5000; ++step) {
+        auto YHat = sinNN.forward(X);
+        // float loss = sinNN.compute_loss(Y, YHat);
+        // if (step % 50 == 0) std::cout << "step " << step << " loss " << loss << "\n";
+        sinNN.backward(Y, YHat);
+        sinNN.update();
+    }
 
-    result = result.sub(B);
-
-    std::cout << result << std::endl;
-
-    auto transpose = result.transpose();
-
-    std::cout << transpose << std::endl;
-
-    std::cout << transpose.scalarMul(5) << std::endl;
-
-    auto cubed = [](auto x) { return x*x*x;};
-
-    std::cout << transpose.map(cubed) << std::endl;
-
-    std::cout << transpose.sumOverColumns() << std::endl;
-
-    auto bias = Math::Matrix<double>(2, 1, 0);
-    bias(0,0) = 1; bias(1,0) = 2;
-
-    std::cout << transpose.addBias(bias) << std::endl;
-
-    if(test_hadamard_product())
-        std::cout << "Hadamard check works" << std::endl;
-    else
-        std::cout << "Hadamard does not works" << std::endl;
-
-    auto mulA = Math::Matrix<double>(3, 2);
-    auto mulB = Math::Matrix<double>(2, 4);
-
-    mulA(0,0) = 1; mulA(0,1) = 2;
-    mulA(1,0) = 9; mulA(1,1) = 3;
-    mulA(2,0) = 7; mulA(2,1) = 4;
-
-    mulB(0,0) = 1; mulB(0,1) = 6; mulB(0,2) = 3; mulB(0,3) = 8;
-    mulB(1,0) = 4; mulB(1,1) = 9; mulB(1,2) = 2; mulB(1,3) = 3;
-
-    std::cout << mulA << std::endl;
-    std::cout << mulB << std::endl;
-
-    std::cout << mulA.matMul(mulB) << std::endl;
-
+    auto YHat = sinNN.forward(X);
+    std::cout << "final loss " << sinNN.compute_loss(Y, YHat) << "\n";
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start);
     return 0;
 }
