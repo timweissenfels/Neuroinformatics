29eaf8faa94f46e1a9b48371c768d40e7ec9fbde
timweissenfels <timweissenfels7@gmail.com>
2025-11-02 01:49:26 +0100

    Added logicCurcit code + example POC

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include "NeuralNetworks/NeuralNetwork.h"
 #include "NeuralNetworks/LossType.h"
 #include "Math/Matrix.h"
+#include "Misc/generateNNDataLogicCurcit.h"
 
 void sinPOC() {
     auto X = Math::Matrix<float>(1,71,0);
@@ -12,7 +13,7 @@ void sinPOC() {
 
     for(int i = 0; i <= 70; i++) {
         X(0, i) = (float)i/10;
-        Y(0,i) = sinf((float)i/10);
+        Y(0,i) = sinf((float)i/10) + cosf((float)i/10);
     }
 
     NeuralNetworks::NeuralNetwork sinNN(NeuralNetworks::LossType::MSE, (double)0.09, 1000, 32, 42, X);
@@ -23,8 +24,8 @@ void sinPOC() {
 
     for (int step = 0; step < 5000; ++step) {
         auto YHat = sinNN.forward(X);
-        // float loss = sinNN.compute_loss(Y, YHat);
-        // if (step % 50 == 0) std::cout << "step " << step << " loss " << loss << "\n";
+        float loss = sinNN.compute_loss(Y, YHat);
+        if (step % 50 == 0) std::cout << "step " << step << " loss " << loss << "\n";
         sinNN.backward(Y, YHat);
         sinNN.update();
     }
@@ -73,7 +74,68 @@ void xorPOC() {
     std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start);
 }
 
+std::pair<Math::Matrix<float>, Math::Matrix<float>> generateNNDataLogicCurcit() {
+    constexpr unsigned short inputShape = 3;
+    constexpr unsigned short outputShape = 1;
+
+    Math::Matrix<float> X(3,std::pow(2, 3));
+    Math::Matrix<float> Y(1,std::pow(2, 3));
+
+    auto mapper = [](const std::bitset<inputShape>& input) -> std::bitset<outputShape> {
+        std::bitset<1> result;
+        auto term1 = !input[0] && !input[1] && !input[2];
+        auto term2 = !input[0] && input[1] && !input[2];
+        auto term3 = input[2] && input[1] && !input[0];
+        auto term4 = input[0] && input[1] && input[2];
+
+        result[0] =  term1 || term2 || term3 || term4;
+
+        return result;
+    };
+
+    auto data = generateNNData<inputShape, outputShape>(mapper);
+
+    for (std::size_t i = 0; i < data.size(); i++) {
+        std::cout << "Input: " << data.at(i).first << " - Output: " << data.at(i).second << '\n';
+
+        X(0,i) = static_cast<float>(data.at(i).first[0]);
+        X(1,i) = static_cast<float>(data.at(i).first[1]);
+        X(2,i) = static_cast<float>(data.at(i).first[2]);
+        Y(0,i) = data.at(i).second.to_ulong();
+    }
+    return {X,Y};
+}
+
+void logicPOC() {
+    auto data = generateNNDataLogicCurcit();
+    auto X = data.first;
+    auto Y = data.second;
+
+    NeuralNetworks::NeuralNetwork xorNN(NeuralNetworks::LossType::BCE, (double)0.05, 1000, 32, 42, X);
+    xorNN.AddDenseLayer(3,8,NeuralNetworks::ActivationTypes::Tanh);
+    xorNN.AddDenseLayer(8,8,NeuralNetworks::ActivationTypes::Tanh);
+    xorNN.AddDenseLayer(8,1,NeuralNetworks::ActivationTypes::Sigmoid);
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (int step = 0; step < 5000; ++step) {
+        auto YHat = xorNN.forward(X);
+        float loss = xorNN.compute_loss(Y, YHat);
+        if (step % 50 == 0) std::cout << "step " << step << " loss " << loss << "\n";
+        xorNN.backward(Y, YHat);
+        xorNN.update();
+    }
+
+    auto YHat = xorNN.forward(X);
+    std::cout << "final loss " << xorNN.compute_loss(Y, YHat) << "\n";
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start);
+
+}
+
 int main() {
-    xorPOC();
+    sinPOC();
+    // xorPOC();
+    //logicPOC();
+
     return 0;
 }
